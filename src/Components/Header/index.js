24fd165529646e4a60f307aaad11afc66bcf8edc
Giankaz <91921192+Giankaz@users.noreply.github.com>
2022-05-11 11:33:30 -0300

    feat: added header provider and a favIco to the site

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,28 +1,29 @@
 import { StyledHeader } from "./style"
 import leaf from '../../Images/leaf.png'
+import { useHeader } from "../../Providers/Header"
 
 const Header = () => {
+    //const {headerType} = useHeader()
+
     return (
         <StyledHeader>
             <section>
-            <h1>ECO<span>leta</span> <img src={leaf} alt="" /></h1>
-        
-           <nav>
-                <ul>
-                    <li>
-                        Sobre Nós
-                    </li>
-                    <li>
-                        Como Atuamos
-                    </li>   
-                     <li>
-                        Faça Parte
-                    </li>
-                </ul>
-            </nav>  
-            
-            </section>
+                <h1>ECO<span>leta</span> <img src={leaf} alt="" /></h1>
 
+                <nav>
+                    <ul>
+                        <li>
+                            Sobre Nós
+                        </li>
+                        <li>
+                            Como Atuamos
+                        </li>
+                        <li>
+                            Faça Parte
+                        </li>
+                    </ul>
+                </nav>
+            </section>
         </StyledHeader>
     )
 }
