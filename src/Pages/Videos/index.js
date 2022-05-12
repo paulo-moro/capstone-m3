cf36337ccbece3564dac697e4884b2d6d26035cb
Giankaz <91921192+Giankaz@users.noreply.github.com>
2022-05-12 15:29:32 -0300

    feat: initial commit

---
+++ src/Pages/Videos/index.js
@@ -0,0 +1,12 @@
+import ReactPlayer from 'react-player'
+
+function Videos() {
+  return (
+    <>
+        <ReactPlayer url='https://www.youtube.com/watch?v=DnH4Hb1s8D8'/>
+
+    </>
+  )
+}
+
+export default Videos
